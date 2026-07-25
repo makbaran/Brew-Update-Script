fdd17656b20b4a5ce8be7aa2e99ced9bfe751794
Sole <114538195+makbaran@users.noreply.github.com>
2026-07-25 13:06:25 -0500

    feat: add outdated upgrade, add autoremove unneeded packages, added, add cleanup all outdated and orphaned files, lastly add output to user when script completes.

--- brew.sh
+++ brew.sh
@@ -2,11 +2,18 @@
 
 brew update
 echo "update complete"
+brew outdated
+echo "outdated complete"
 brew upgrade
 echo "upgrade complete"
 brew upgrade --cask
 echo "upgrading cask complete"
-brew cleanup
+brew autoremove
+echo "autoremove complete"
+brew cleanup --prune=all
 echo "cleanup complete"
 brew doctor
 echo "doctor complete"
+
+echo "brew update script complete"
+echo "Time to brew it up!"
